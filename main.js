089978fe80f7f5e68c820e912e30685ee8a3c86b
AOsterbaan <alexosterbaan@hotmail.com>
2025-08-21 12:50:03 -0600

    Update main.js

--- main.js
+++ main.js
@@ -265,7 +265,7 @@ function getPanelWidth(guiObject) {
 // ----------------------------
 function computeCanvasSize() {
   const PAD_SIDE = 20;
-  const aspect = 16 / 9;
+  const aspect = 4/3;
 
   let availableWidth;
   let maxHeight;
